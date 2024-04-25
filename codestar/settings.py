c9f06752356c8de8bd5c7eacc28a866cf2f336e5
Ben <bengilbertmusic@gmail.com>
2024-04-25 11:48:48 +0000

    Edit STATIC_ROOT to fix deployment error

--- codestar/settings.py
+++ codestar/settings.py
@@ -117,7 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Define the directory where Django will collect static files during deployment
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
