c42943eaf575a3789c3ce34d35384d5a65babd97
Ben <bengilbertmusic@gmail.com>
2024-04-25 16:16:20 +0000

    Built the database model

--- codestar/settings.py
+++ codestar/settings.py
@@ -91,6 +91,12 @@ DATABASES = {
 }
 
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
