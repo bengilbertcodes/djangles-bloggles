970df6a691db3b841716188841211f69cdb0fd83
Ben <bengilbertmusic@gmail.com>
2024-04-25 11:50:54 +0000

    Comment out STATIC_ROOT

--- codestar/settings.py
+++ codestar/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Define the directory where Django will collect static files during deployment
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
