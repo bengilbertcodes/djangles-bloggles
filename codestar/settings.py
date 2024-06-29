fbd45137b9abf30325f306901891054d74e8f2a9
Ben Gilbert <bengilbertmusic@gmail.com>
2024-06-29 14:47:13 +0000

    Add custom CSS

--- codestar/settings.py
+++ codestar/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Define the directory where Django will collect static files during deployment
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
