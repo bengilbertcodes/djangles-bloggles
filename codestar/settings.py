6ea8cf84e2af7f3d02630d2a07d914a993585017
Ben Gilbert <bengilbertmusic@gmail.com>
2024-07-11 19:52:02 +0000

    Added a comma

--- codestar/settings.py
+++ codestar/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.gitpod.io",
-    "https://*.herokuapp.com"
+    "https://*.herokuapp.com",
     'https://8000-bengilbertc-djanglesblo-a97i712q6k7.ws.codeinstitute-ide.net',
 ]
 
